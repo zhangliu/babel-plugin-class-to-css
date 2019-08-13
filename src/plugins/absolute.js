753b1cdcac4eace51905b0e6c88d715e75f6dc3b
zhangliu <zhangliuge@yeah.net>
2019-08-13 11:38:43 +0800

    modify

--- src/plugins/absolute.js
+++ src/plugins/absolute.js
@@ -1,13 +1,12 @@
 const handle = (names, path) => {
   if (names.length <= 0) return names
-  if (path.node.arguments[0].value !== 'span') return names
 
-  // 判断是否有 margin 和 padding 的设置
-  const reg = /^[mp][trbl]{0,1}\d+$/
+  // 判断是否有 top/right/bottom/left 和 padding 的设置
+  const reg = /^[trbl]{1,1}\d+$/
   let name = names.find(n => reg.test(n))
   if (!name) return names
 
-  names.unshift('dib')
+  names.unshift('pa')
 
   const uniqNames = [...(new Set(names))]
   const arg = path.node.arguments[1]
