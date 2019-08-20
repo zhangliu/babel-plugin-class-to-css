f8fbbdfa45dede888f3aed5cb49052f9ddcf4608
zhangliu <zhangliuge@yeah.net>
2019-08-20 15:50:46 +0800

    modify

--- src/converts/backgroundImg.js
+++ src/converts/backgroundImg.js
@@ -2,9 +2,11 @@ module.exports = (name, opts) => {
   const url = opts.imgUrl
   if (!url) return
 
-  const match = name.match(/^bgi([0-9a-zA-Z]+)-([0-9a-zA-Z]+)($|_.*)/)
-  if (!match) return
-  if (!match[1] || !match[2]) return
+  const match = name.match(/^bgi-([_0-9a-zA-Z]+)(-([0-9a-zA-Z]+))?($|_.*)/)
+  if (!match || !match[1]) return
 
-  return `background-image: url('${url}/${match[1]}.${match[2]}')`
+  const name = match[1]
+  const type = match[3] || 'png'
+
+  return `background-image: url('${url}/${name}.${type}')`
 }
